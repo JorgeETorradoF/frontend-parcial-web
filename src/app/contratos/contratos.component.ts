9fac1b38bc6624ead05d5fb8958aff1a3774fdc8
Jorge Torrado <jorgeetorradoforero@gmail.com>
2024-11-19 17:41:26 -0500

    más cambios

--- src/app/contratos/contratos.component.ts
+++ src/app/contratos/contratos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ContratoService } from '../services/contrato.service';  // Importa el servicio correcto
+import { ContratoService } from '../services/contrato.service'; 
 
 @Component({
   selector: 'app-contrato',
@@ -8,26 +8,44 @@ import { ContratoService } from '../services/contrato.service';  // Importa el s
 })
 export class ContratoComponent implements OnInit {
   contratos: any[] = [];
-  nuevoContrato: any = { nombre: '', descripcion: '' };  // Suponiendo que los contratos tienen estos campos
+  nuevoContrato: any = { 
+    identificador: '',
+    precio: null,
+    nombreContratante: '',
+    documentoContratante: '',
+    nombreContratantista: '',
+    documentoContratantista: '',
+    fechaInicial: '',
+    fechaFinal: ''
+  };
 
   constructor(private contratoService: ContratoService) { }
 
   ngOnInit(): void {
-    this.cargarContratos();  // Cargar contratos al inicio
+    this.cargarContratos(); 
   }
 
-  // Cargar todos los contratos desde el backend
+
   cargarContratos(): void {
     this.contratoService.obtenerTodosLosContratos().subscribe((data: any[]) => {
       this.contratos = data;
     });
   }
 
-  // Insertar un nuevo contrato en la base de datos
+
   agregarContrato(): void {
     this.contratoService.insertarContrato(this.nuevoContrato).subscribe((data: any) => {
-      this.cargarContratos();  // Recargar los contratos
-      this.nuevoContrato = { nombre: '', descripcion: '' };  // Limpiar el formulario
+      this.cargarContratos();  
+      this.nuevoContrato = { 
+        identificador: '',
+        precio: null,
+        nombreContratante: '',
+        documentoContratante: '',
+        nombreContratantista: '',
+        documentoContratantista: '',
+        fechaInicial: '',
+        fechaFinal: ''
+      };
     });
   }
 }
